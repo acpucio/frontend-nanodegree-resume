8726155fbe2050b938ea6d5ffe38ffe24ecc62a1
Pucio <acpucio@gmail.com>
2016-02-03 00:56:40 -0500

    Correct resumebuiler.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -143,14 +143,14 @@ for (job in work.jobs){
 	$("#workExperience").append(HTMLworkStart);
 	// concat employer and title
 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replacy("%data%", work.jobs[job].title);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 	var formattedEmployerTitle = formattedEmployer + formattedTitle;
 	$(".work-entry:last").append(formattedEmployerTitle);
 
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates),
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
 	$(".work-entry:last").append(formattedDates);
 
-	var formattedDescription = HTMLworkDescription.replace(%data%, work.jobs[job].description);
-	$("work-entry:last").append(formattedDescription);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
 }
 	
